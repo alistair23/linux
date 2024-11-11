da0b1caa5cedbb5776718436d6e597fd2b345c55
Alistair Francis <alistair@alistair23.me>
2024-11-11 14:50:03 +1000

    rust: helpers: Remove some task helpers
    Now that we support wrap-static-fns we no longer need the custom helpers.
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>
    Tested-by: Dirk Behme <dirk.behme@de.bosch.com>

--- rust/helpers/task.c
+++ rust/helpers/task.c
@@ -7,16 +7,6 @@ struct task_struct *rust_helper_get_current(void)
 	return current;
 }
 
-void rust_helper_get_task_struct(struct task_struct *t)
-{
-	get_task_struct(t);
-}
-
-void rust_helper_put_task_struct(struct task_struct *t)
-{
-	put_task_struct(t);
-}
-
 kuid_t rust_helper_task_uid(struct task_struct *task)
 {
 	return task_uid(task);
