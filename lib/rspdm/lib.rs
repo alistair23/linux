9e523b1223f05fd3b8ed1dc5bdd7556df114544d
Alistair Francis <alistair.francis@wdc.com>
2025-02-05 11:48:18 +1000

    lib: rspdm: Support SPDM get_digests
    Signed-off-by: Alistair Francis <alistair.francis@wdc.com>

--- lib/rspdm/lib.rs
+++ lib/rspdm/lib.rs
@@ -122,6 +122,10 @@ pub unsafe extern "C" fn spdm_authenticate(state: &'static mut SpdmState) -> c_i
         return e.to_errno() as c_int;
     }
 
+    if let Err(e) = state.get_digests() {
+        return e.to_errno() as c_int;
+    }
+
     0
 }
 
