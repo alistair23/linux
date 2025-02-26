650bf3b0d19cd8a271266dc9b42fc61409050c7f
Alistair Francis <alistair@alistair23.me>
2025-02-26 18:51:02 +1000

    rust: allow extracting the buffer from a CString
    The kernel CString is a wrapper aroud a KVec. This patch allows
    retrieving the underlying buffer and consuming the CString. This allows
    users to create a CString from a string and then retrieve the underlying
    buffer.
    
    Signed-off-by: Alistair Francis <alistair@alistair23.me>

--- rust/kernel/str.rs
+++ rust/kernel/str.rs
@@ -844,6 +844,11 @@ impl CString {
         // exist in the buffer.
         Ok(Self { buf })
     }
+
+    /// Return the internal buffer while consuming the original [`CString`]
+    pub fn into_vec(self) -> KVec<u8> {
+        self.buf
+    }
 }
 
 impl Deref for CString {
